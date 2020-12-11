31749d1d1d4afe783023dd8c6355367bb53859e7
matthewsteinhart <steinmat@iu.edu>
2020-12-11 11:52:41 -0500

    Update util_functions.R
    added some comments

--- code/util_functions.R
+++ code/util_functions.R
@@ -45,9 +45,13 @@ square_it <- function(x){
   sq <- x*x
   return(sq)
 }
-
+# Square function
+# adapted from https://hbctraining.github.io/Intro-to-R/lessons/03_introR-functions-and-arguments.html#user-defined-functions
+# and https://www.r-bloggers.com/how-to-write-and-debug-an-r-function/
 
 # Anscombe's quartet
+# Anscombe's quartet
+# Examples from https://www.r-bloggers.com/using-and-abusing-data-visualization-anscombes-quartet-and-cheating-bonferroni/
 anscombes_quartet <- {
     library(Tmisc)
 
